4949d7299dc7a4ed45552ceda7c3703f59026977
Akhildevaki <80966504+Akhildevaki@users.noreply.github.com>
2022-08-31 19:40:12 -0500

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,17 @@
+# creating brothers tuple
+
+brothers = ('virat','dhoni', 'sachin')
+
+# creating sisters tuple
+sisters = ('sonu','dusky','pinky')
+
+# joining two tuples
+siblings = brothers + sisters
+print(siblings)
+
+# Length of siblings
+print(len(siblings))
+
+# Added mother and father name by modifying the siblings tuple
+family_members = siblings+("hrithik","alia")
+print(family_members)
